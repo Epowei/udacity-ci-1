e4f226f8b302439968dc9b00ce6a6cf146b02245
epowei <erejef@yahoo.com>
2023-01-15 15:30:52 +0100

    circleci infrastructure code.

--- src/messages.test.ts
+++
@@ -1,6 +0,0 @@
-import { generateMessage } from "./messages";
-
-test("generates the message correctly", () => {
-  const message = generateMessage();
-  expect(message).toBe("Hello Byron");
-});
--- src/messages.ts
+++
@@ -1,3 +0,0 @@
-export function generateMessage() {
-  return "Hello Byron";
-}
